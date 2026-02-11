32f0abbebc2237af492bdd31ab1645aaf0287431
Agonis Shala <agonis-shala@hotmail.fr>
2026-02-11 20:55:54 +0100

    Refactor project structure

--- src/main/java/com/securetask/Application.java
+++ src/main/java/com/securetask/Application.java
@@ -1,13 +1,13 @@
-package com.securetask.secure_task_manager;
+package com.securetask;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SecureTaskManagerApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SecureTaskManagerApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
