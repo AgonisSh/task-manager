f80ebaa11bcfe166deedec462922bec393b49fd2
Agonis Shala <agonis-shala@hotmail.fr>
2026-02-11 16:34:47 +0100

    Initial commit with SpringBoot 3.5 setup

---
+++ src/test/java/com/securetask/secure_task_manager/SecureTaskManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.securetask.secure_task_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureTaskManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
