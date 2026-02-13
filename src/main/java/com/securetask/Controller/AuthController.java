e5a0336062f86cb60c68623de49d6b8665c805b0
Agonis Shala <agonis-shala@hotmail.fr>
2026-02-13 18:58:23 +0100

    feat(auth) add user registration endpoint and service logic

--- src/main/java/com/securetask/Controller/AuthController.java
+++ src/main/java/com/securetask/Controller/AuthController.java
@@ -4,10 +4,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.securetask.DTO.requests.AuthRequest;
+import com.securetask.DTO.requests.RegisterRequest;
 import com.securetask.DTO.responses.AuthResponse;
+import com.securetask.DTO.responses.UserResponse;
 import com.securetask.Service.auth.AuthService;
 
 import lombok.RequiredArgsConstructor;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -22,5 +27,12 @@ public class AuthController {
     @PostMapping("/login")
     public AuthResponse login(@RequestBody AuthRequest authRequest) {
         return authService.authenticate(authRequest);
+    }
+
+
+    @PostMapping("/register")
+    public ResponseEntity<UserResponse> registerUser(@RequestBody RegisterRequest userRequest) 
+    {
+        return new ResponseEntity<>(authService.register(userRequest), HttpStatus.CREATED);
     }
 }
--- src/main/java/com/securetask/Service/auth/AuthService.java
+++ src/main/java/com/securetask/Service/auth/AuthService.java
@@ -6,8 +6,15 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
+import com.securetask.DAO.UserDAO;
 import com.securetask.DTO.requests.AuthRequest;
+import com.securetask.DTO.requests.RegisterRequest;
 import com.securetask.DTO.responses.AuthResponse;
+import com.securetask.DTO.responses.UserResponse;
+import com.securetask.Entitity.User;
+import com.securetask.Factory.UserFactory;
+import com.securetask.Mapper.UserMapper;
+import com.securetask.Validator.UserValidator;
 
 
 @Service
@@ -16,6 +23,10 @@ public class AuthService {
 
     private final AuthenticationManager authenticationManager;
     private final JwtTokenService jwtTokenService;
+    private final UserDAO userDAO;
+    private final UserValidator userValidator;
+    private final UserMapper userMapper;
+    private final UserFactory userFactory;
 
 
     public AuthResponse authenticate(AuthRequest authRequest) 
@@ -27,5 +38,14 @@ public class AuthService {
         Long expiresAt = jwtTokenService.extractExpirationTime(jwtToken);
 
         return new AuthResponse(jwtToken, authentication.getName(), expiresAt);
+    }
+
+
+    public UserResponse register(RegisterRequest userRequest) 
+    {
+        userValidator.validateUniqueFields(userRequest);
+        User user = userFactory.createUser(userRequest, User.Role.USER);
+
+        return userMapper.toRegisterResponse(userDAO.save(user));
     }
 }
