0f43d5ca768593e90592a4c5cb1d34503841a3e6
Agonis Shala <agonis-shala@hotmail.fr>
2026-02-16 18:43:31 +0100

    refactor(user) remove unused import statements in User entity

--- src/main/java/com/securetask/Entitity/User.java
+++ src/main/java/com/securetask/Entitity/User.java
@@ -2,8 +2,6 @@ package com.securetask.Entitity;
 
 import java.time.LocalDateTime;
 
-import com.securetask.Entitity.auth.RefreshToken;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
@@ -11,7 +9,6 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToOne;
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
