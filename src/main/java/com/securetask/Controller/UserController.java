bc00280e9f937fdf965a9696dc08713222c7bf12
Agonis Shala <agonis-shala@hotmail.fr>
2026-02-16 18:43:55 +0100

    refactor(user) remove unused import statements in UserController

--- src/main/java/com/securetask/Controller/UserController.java
+++ src/main/java/com/securetask/Controller/UserController.java
@@ -1,15 +1,8 @@
 package com.securetask.Controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.securetask.DTO.requests.RegisterRequest;
-import com.securetask.DTO.responses.UserResponse;
 import com.securetask.Service.UserService;
 
 
