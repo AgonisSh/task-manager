0bbe400aee0d76a07da2e3ea5470e8c27b29b38e
Agonis Shala <agonis-shala@hotmail.fr>
2026-02-18 17:21:52 +0100

    feat(tests) add H2 database dependency for in memory testing

--- backend/src/test/java/com/securetask/Authentication/AuthControllerMockMvcTest.java
+++ backend/src/test/java/com/securetask/Authentication/AuthControllerMockMvcTest.java
@@ -5,13 +5,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.securetask.DTO.requests.AuthRequest;
 import com.securetask.DTO.requests.RegisterRequest;
 
-import jakarta.transaction.Transactional;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -20,7 +21,8 @@ import java.util.Objects;
 
 @AutoConfigureMockMvc
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@Transactional
+@ActiveProfiles("test") // Use application-test.yml for testing
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD) // Reset DB before each test
 public class AuthControllerMockMvcTest {
 
 
